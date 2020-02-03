961445c61d3c216205cb0f0934bf6d9dc576767f
Håvard Langdal <havardlangdal@gmail.com>
2020-02-03 14:54:25 +0100

    Backup commit

--- weights_visualization.py
+++ weights_visualization.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.legend_handler import HandlerLine2D
 from scipy.interpolate import make_interp_spline, BSpline
+import matplotlib.patches as mpatches
 
 
 def _get_weights(filename):
@@ -11,6 +13,22 @@ def _get_weights(filename):
             weights[i] = [float(current_weights[0]), float(current_weights[1])]
             # print(weights[i])
     return weights
+
+
+def _get_compact_weights(filename):
+    weights = []
+    frequency = []
+    discrete_time = []
+    with open(filename, "r", encoding="utf-8") as weights_file:
+        for i, weights_triple in enumerate(weights_file):
+            current_weights = weights_triple.replace(",", ".").split("\t")
+            weights.append([current_weights[1], current_weights[2]])
+            discrete_time.append(int(float(current_weights[0].strip())))
+            curr_frequency = int(float(current_weights[2].strip()))
+            frequency.append(curr_frequency)
+            for k in range(0, curr_frequency):
+                weights.append([current_weights[0], current_weights[1]])
+    return discrete_time, weights
 
 
 def plot_weights(filename):
@@ -28,5 +46,18 @@ def plot_smooth_weights(filename):
     plt.show()
 
 
+def plot_weights_with_compact_format(filename):
+    _disc_time, _weights = _get_compact_weights(filename)
+    print("Num sampes: ", len(_weights))
+    weights = np.asarray(_weights[:], dtype=np.float32)
+    labels = ['lru', 'lfu']
+    shape = ['r-', 'b-']
+    for col, lb, shape in zip(weights.T, labels, shape):
+        plot = plt.plot(_disc_time, col, shape, label=lb)
+        # plt.legend(handler_map={plot: HandlerLine2D(numpoints=4)})
+
+    plt.show()
+
+
 if __name__ == '__main__':
-    plot_weights("C:/Users/havar/Home/caffeine/weights.csv")
+    plot_weights_with_compact_format("C:/Users/havar/Home/cache_simulation_results/weights13.csv")
