24f03a67ef60fa5cc74da062643383dbe121ed33
Håvard Langdal <havardlangdal@gmail.com>
2020-03-13 19:04:47 +0100

    Backup commit

--- plot_hit_rates.py
+++ plot_hit_rates.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d, UnivariateSpline
+import pandas as pd
 
 
 def _read_hit_rates(filename, limit=None):
@@ -74,16 +75,55 @@ def running_mean(x, N):
     return (cumsum[N:] - cumsum[:-N]) / float(N)
 
 
+def _plot_weighted_hit_rates(filename):
+    df = pd.read_csv(filename, delimiter=',')
+    # print(df[['Policy', 'Hit rate','Weighted Hit Rate']])
+    # print(df["Policy"].to_string(index=False))
+    # print("HR\n",df["Hit rate"].to_string(index=False))
+    # df[["Policy", "Hit rate"]].to_csv("hr.csv")
+    # df[["Policy", "Weighted Hit Rate"]].to_csv("whr.csv")
+
+    lfu_filter = df["Policy"].str.contains("linked.Lfu")
+    lru_filter = df["Policy"].str.contains("linked.Lru")
+    tiny_lfu_filter = df["Policy"].str.contains("TinyLfu" and "linked")
+    admit_filter = df["Admit rate"] != "100,00"
+
+    threshold = df.where(lfu_filter | lru_filter).dropna()
+    print(threshold.to_string(index=False))
+    threshold.to_csv("res.csv", index=False)
+
+
+def shit_assaa(filename, query=None):
+    df = pd.read_csv(filename, delimiter=',')
+    lfu_fiter = df["Policy"].str.contains("linked.Lfu_ThresholdAdmittor")
+    first_filter = []
+    for i in range(0, df.shape[0]):
+        bool_set = i < 30
+        first_filter.append(bool_set)
+    data = df.where(lfu_fiter | first_filter).dropna()
+    print(data)
+    data.to_csv("thresholds.csv", index=False)
+
+
+def plot_hitrate_csv(filename):
+    df = pd.read_csv(filename)
+    df.plot.bar(index=["Policy"], values=["Hit rate"])
+    # plt.plot(df["Policy"], df["Hit rate"])
+    # plt.show()
+
+
 if __name__ == '__main__':
     basedir = "C:/Users/havar/Home/cache_simulation_results/"
 
     # _plot_hit_rates_with_flagged(basedir + "res_01_r.csv")
-    _plot_hit_rates([basedir + "res_LR_r.csv", basedir + "res_LR_W_r.csv", basedir + "res_None_r.csv", basedir + "arc_r.csv"],
-                    ["LR", "LR_W", "None", "ARC"], None)
+    # _plot_hit_rates([basedir + "res_LR_r.csv", basedir + "res_LR_W_r.csv", basedir + "res_None_r.csv", basedir + "arc_r.csv"],
+    #                ["LR", "LR_W", "None", "ARC"], None)
     # _plot_hit_rates([basedir + "res_None_r.csv"], None)
 
     mylist = [1, 2, 3, 4, 5, 6, 7]
     print(running_mean(np.asarray(mylist), 4))
+
+    plot_hitrate_csv(basedir + "Threshold_mds0_x_graph.csv")
 
     '''
         colors = [0,0,0,1,0,1] #red is 0, blue is 1
